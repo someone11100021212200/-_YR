6ff3b0c66042470753266c2ae055c4d4d05b1784
someone11100021212200 <52664781+someone11100021212200@users.noreply.github.com>
2019-07-18 12:33:42 +0000

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const devs = ['389090790984515594'];
 const db = require('quick.db');
-const premium = ['451229433483755521']
+const premium = ['470896018603376640']
 const client = new Discord.Client();   
 const bot = new Discord.Client();   
 const giphy = require('giphy-api')();    
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['-help','Type ahelp'];
+    var setGame = ['ahelp','Type ahelp'];
     var i = -1;
     var j = 0;
     setInterval(function (){
